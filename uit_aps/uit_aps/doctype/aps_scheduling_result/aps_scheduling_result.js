81b5aaa3343ed047d6d2b8742be629012c5cab63
thanhnc <thanhnc@digitalbiz.com.vn>
2026-01-06 01:15:49 +0700

    readme function

--- uit_aps/uit_aps/doctype/aps_scheduling_result/aps_scheduling_result.js
+++ uit_aps/uit_aps/doctype/aps_scheduling_result/aps_scheduling_result.js
@@ -1,8 +1,44 @@
 // Copyright (c) 2025, thanhnc and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("APS Scheduling Result", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.listview_settings["APS Scheduling Result"] = {
+	// Them field subject vao data de Gantt view co the hien thi
+	onload: function(listview) {
+		listview.page.add_inner_button(__("Gantt"), function() {
+			frappe.set_route("List", "APS Scheduling Result", "Gantt");
+		});
+		
+		// Override prepare_tasks de tinh toan title
+		if (listview.view_name === "Gantt") {
+			const original_prepare_tasks = listview.prepare_tasks;
+			listview.prepare_tasks = function() {
+				original_prepare_tasks.call(this);
+				
+				// Tinh toan subject cho moi task
+				this.tasks = this.tasks.map((task) => {
+					const item = this.data.find((d) => d.name === task.id);
+					if (item) {
+						const subject_parts = [];
+						if (item.job_card) {
+							subject_parts.push(`Job Card: ${item.job_card}`);
+						}
+						if (item.workstation) {
+							subject_parts.push(`Workstation: ${item.workstation}`);
+						}
+						if (item.operation) {
+							subject_parts.push(`Operation: ${item.operation}`);
+						}
+						if (item.is_late && item.delay_reason) {
+							subject_parts.push(`⚠️ ${item.delay_reason}`);
+						}
+						
+						task.name = subject_parts.length > 0 
+							? subject_parts.join(" | ") 
+							: item.name;
+					}
+					return task;
+				});
+			};
+		}
+	}
+};
